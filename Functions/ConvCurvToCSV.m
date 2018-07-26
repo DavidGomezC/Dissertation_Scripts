37fb26d12aa95fe36ac723d1336f923664a7c889
DavidGomezC <38907407+DavidGomezC@users.noreply.github.com>
2018-07-26 22:58:58 +0100

    Update ConvCurvToCSV.m

--- Functions/ConvCurvToCSV.m
+++ Functions/ConvCurvToCSV.m
@@ -11,11 +11,18 @@ end
 cf = {};
 fe = {};
 for x=1:length(list2(:,1))
-    load(list2(x,:));
-    s = oed_results{1}.nlpsol.neval;
-    c = oed_results{1}.nlpsol.f;
-    cf{x} = c;
-    fe{x} = s;
+    if (contains(list2(x,:),'eSS'))
+        load(list2(x,:));
+        s = oed_results{1}.nlpsol.neval;
+        c = oed_results{1}.nlpsol.f;
+        cf{x} = c; fe{x} = s;
+    elseif (contains(list2(x,:),'DE'))
+        load('FunctionEvaluationsVector');
+        load(list2(x,:));       
+        s = FE;
+        c = oed_results{1}.nlpsol.conv_curve(:,2);
+        cf{x} = c.'; fe{x} = s;
+    end
 end
 
 for x=1:length(cf)
